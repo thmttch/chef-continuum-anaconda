4480c123a61f64545df72f5d04b2e6a36e4e3d78
Matt Chu <matt.chu@gmail.com>
2014-04-11 09:24:58 -0700

    initial working version

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,12 @@
 name             'chef-continuum-anaconda'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Matt Chu'
+maintainer_email 'matt.chu@gmail.com'
+license          'MIT'
 description      'Installs/Configures chef-continuum-anaconda'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+supports 'ubuntu', '= 12.04'
+
+depends 'apt'
+depends 'python'
