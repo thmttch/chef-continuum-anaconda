e91dc1d3e237bae003526ab21baba1c70e6bce45
Matt Chu <matt.chu@gmail.com>
2014-06-23 14:11:47 -0700

    upgrade to anaconda 2.0 as the default. misc cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,6 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
